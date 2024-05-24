a720de4a20cf4b9fb5a3dd5df39922216b129552
Rodrigo Silva <112216918+rodericussilva@users.noreply.github.com>
2024-05-24 16:46:12 -0300

    update

---
+++ firebaseInit.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDvUXZ4WoOH3S4jnSBeKUp15TRHgB780nU",
+    authDomain: "controle-gastos-fa8d9.firebaseapp.com",
+    projectId: "controle-gastos-fa8d9",
+    storageBucket: "controle-gastos-fa8d9.appspot.com",
+    messagingSenderId: "802066167205",
+    appId: "1:802066167205:web:dff2a35e2daff527a4644c"
+};
+firebase.inicializeApp(firebaseConfig);
--- index.js
+++ index.js
@@ -27,6 +27,17 @@ function register() {
     window.location.href = "pages/register/register.html";
 }
 
+function recoverPassword() {
+    showLoading();
+    firebase.auth().sendPasswordResetEmail(form.email().value).then(() => {
+        hideLoading();
+        alert('Email enviado com sucesso');
+    }).catch(error => {
+        hideLoading();
+        alert(getErrorMessage(error));
+    });
+}
+
 function getErrorMessage(error) {
     if (error.code == "auth/user-not-found") {
         return "Usuário nao encontrado";
